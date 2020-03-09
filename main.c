546f0614bb26f8eef57f9784cf1740bea7c916f4
Monstarw <596981832@qq.com>
2020-03-09 11:37:17 +0800

    added main.c that can create and print an array of 90 random numbers

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+
+int main(){
+
+	/*初始化乱序数组*/
+	
+	int random_numbers[90];
+	int i;
+	
+	//初始化随机数发生器
+	time_t t;
+	srand((unsigned) time(&t));
+	
+	for(i = 0; i < 90; i++){
+		random_numbers[i] = rand();
+		printf("%d\n", random_numbers[i]);
+	}
+	return 0;
+}
