17220130efee8ebda3053d0ec48c439e07c21a4e
Monstarw <596981832@qq.com>
2020-03-13 17:06:47 +0800

    调试多线程排序

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 #include <pthread.h>
 #include <time.h>
 #include <stdbool.h>
-#define kNumberCount 10	//定义乱序数组中元素个数
+#define kNumberCount 30	//定义乱序数组中元素个数
 #define kNumberCap 100	//定义乱序数组中元素值的上限
-#define kThreadCount 1
+#define kThreadCount 3
 
 typedef struct parameters{
 	int (* numbers)[kNumberCount];
@@ -19,13 +19,13 @@ void * insertion_sort(void * params);
 
 int main(){
 	int random_numbers[kNumberCount];
-	int i, j = 0;
+	int i, j = 0, k;
 	//int thread_count;	//使用的线程个数
 	
 	srand((unsigned) time(0));	//初始化随机数生成种子
 	for(i = 0; i < kNumberCount; i++){	//初始化乱序数组
 		random_numbers[i] = rand() % kNumberCap;
-		printf("%d\t", random_numbers[i]);
+		printf("%d\n", random_numbers[i]);
 	}
 	
 	//printf("请输入使用的线程个数(3/5/6):");
@@ -34,7 +34,6 @@ int main(){
 	parameters * param[kThreadCount];
 	for(i = 0; i < kThreadCount; i++){
 		param[i] = (parameters *) malloc(sizeof(parameters));
-		//parameters * param0 = (parameters *) malloc(sizeof(parameters));
 		param[i]->numbers = &random_numbers;
 		param[i]->start = j;
 		j = j + kNumberCount / kThreadCount;
@@ -44,25 +43,20 @@ int main(){
 	pthread_t thread[kThreadCount];
 	
 	for(i = 0; i < kThreadCount; i++){
-		pthread_create(&thread[i], NULL, insertion_sort, (void *) param[i]);
+		pthread_create(&thread[i], NULL, bubble_sort, (void *) param[i]);
 	}
 	for(i = 0; i < kThreadCount; i++){
 		pthread_join(thread[i], NULL);
 	}
 	
-	//调用冒泡排序
-	//bubble_sort(random_numbers, 0, kNumberCount);
-	
-	//调用选择排序
-	//selection_sort(random_numbers, 0, kNumberCount);
-	
-	//调用插入排序
-	//insertion_sort(random_numbers, 0, kNumberCount);
-	
 	//输出排序结果
 	printf("\n\n");
-	for(i = 0; i < kNumberCount; i++){
-		printf("%d\t", random_numbers[i]);
+	for(i = 0; i < kThreadCount; i++){
+		for(k = 0; k < kNumberCount / kThreadCount; k++){
+			printf("%d\t", random_numbers[j]);
+			j++;
+		}
+		printf("\n");
 	}
 	printf("\n");
 	return 0;
